9507e2da81cd760b90cbea5076899151b7d82227
Yousef Fatouraee <fatouraee96@gmail.com>
2026-07-26 21:14:32 -0400

    Refactor exports in index.ts and remove unused types.ts file

--- src/core/common/types.ts
+++
@@ -1,2 +0,0 @@
-export type { IP, IPv4, IPv6, PublicIPv4, PrivateIPv4, PublicIPv6, PrivateIPv6, PublicIP, PrivateIP } from "./ip";
-export { isIP, isIPv4, isIPv6, isPublicIP, isPrivateIP } from "./ip";
